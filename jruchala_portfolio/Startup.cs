465f0a141359f012e29951185e3bdda4322cb46e
jruchala <jruchala@gmail.com>
2016-08-16 16:49:11 -0400

    initial commit of portfolio website

---
+++ jruchala_portfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jruchala_portfolio.Startup))]
+namespace jruchala_portfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
